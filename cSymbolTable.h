0f576ba27517a6ba56f0f580d221787e1e7cabd0
Theodore Gwynn <theo.gwynn@loki.labs.cset.oit.edu>
2026-01-15 11:03:42 -0800

    renamed ProcessID to more useful name

--- cSymbolTable.h
+++ cSymbolTable.h
@@ -48,54 +48,5 @@ class cSymbolTable
     private:
         std::stack<symbolTable_t*> tables;
 };
-
-/*
-cSymbolTable::cSymbolTable()
-{
-}
-
-symbolTable_t* cSymbolTable::IncreaseScope()
-{
-    symbolTable_t* newScope = new symbolTable_t();
-    tables.push(newScope);
-    return newScope;
-}
-
-symbolTable_t* cSymbolTable::DecreaseScope()
-{
-    symbolTable_t* outerMostScope = tables.pop();
-    return outerMostScope;
-}
-void cSymbolTable::Insert(cSymbol * sym)
-{
-    symbolTable_t* outerScope = tables.pop();
-    outerScope.symbols.insert(std::pair<string, cSymbol>(sym.GetName, sym));
-    tables.push(outerScope);
-    return;
-}
-
-cSymbol* cSymbolTable::Find(string name)
-{
-
-    return nullptr;
-}
-
-cSymbol* cSymbolTable::FindLocal(string name)
-{
-    symbolTable_t* scope = tables.pop();
-    cSymbol* sym = nullptr;
-    try
-    {
-        sym = scope.symbols.at(name);
-    }
-    catch(std::exception e)
-    {
-        sym = nullptr;
-        std::cout<< e.What() << std::endl;
-    }
-    tables.push(scope);
-    return sym;
-}
-*/
 // declare the global symbol table. The definition will have to be in a cpp file
 extern cSymbolTable g_symbolTable;
